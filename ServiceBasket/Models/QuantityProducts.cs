26ce5bb62e2faec6204c7b246aaad52f26656d56
Evgeny Lifanov <lifanov.j@yandex.ru>
2023-02-22 11:42:43 +0300

    Добавление моделей продукта и кол-ва продукта в корзине

---
+++ ServiceBasket/Models/QuantityProducts.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceBasket.Models
+{
+    public class QuantityProducts
+    {
+        public int ProductId { get; set;}
+        public string ProductName { get; set;}
+        public int Quantity { get; set;}
+    }
+}
