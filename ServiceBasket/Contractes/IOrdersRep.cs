a6d3804c000a6733ce80ef6860eff6f1bc7fbe2e
Evgeny Lifanov <lifanov.j@yandex.ru>
2023-02-22 14:12:21 +0300

    Добавление интерфейсов

---
+++ ServiceBasket/Contractes/IOrdersRep.cs
@@ -0,0 +1,17 @@
+﻿using ServiceBasket.Models;
+
+namespace ServiceBasket.Contractes
+{
+    public interface IOrdersRep
+    {
+        Task<Order> GetOrderAsync(int orderId);
+
+        Task<IEnumerable<Order>> GetOrdersAsync();
+
+        Task<Order> AddOrderAsync(Product product);
+
+        Task<Order> AddProductsOrderAsync(int orderId, Product product);
+
+        Task<bool> DeleteOrderAsync(int orderId);
+    }
+}
---
+++ ServiceBasket/Contractes/IProductesRep.cs
@@ -0,0 +1,34 @@
+﻿using ServiceBasket.Models;
+
+namespace ServiceBasket.Contractes
+{
+    public interface IProductesRep
+    {
+        /// <summary>
+        /// Получение одного товара 
+        /// </summary>
+        /// <param name="productId">Номер товара</param>
+        /// <returns></returns>
+        Task<Product> GetProductAsync(int productId);
+
+        /// <summary>
+        /// Получение списка товаров
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<Product>> GetProductsAsync();
+
+        /// <summary>
+        /// Добавление товара
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        Task<Product> AddProductAsync(Product product);
+
+        /// <summary>
+        /// Удаление товара
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
+        Task<bool> DeleteProductAsync(int productId);
+    }
+}
